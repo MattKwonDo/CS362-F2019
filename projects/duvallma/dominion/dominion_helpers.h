d7e02054e7690604dfbb7595238ad5b378fadeae
Matt <duvallma@oregonstate.edu>
2019-10-13 19:20:34 -0500

    refactor code from switch statement to 5 new functions

--- projects/duvallma/dominion/dominion_helpers.h
+++ projects/duvallma/dominion/dominion_helpers.h
@@ -9,7 +9,15 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
                 int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+
+int playCardBaron(int choice1, struct gameState *state, int currentPlayer);
+int playCardMinion(int choice1, int choice2, struct gameState *state, int currentPlayer, int handPos);
+int playCardAmbassador(int choice1, int choice2, int handPos, struct gameState *state, int currentPlayer);
+int playCardTribute(int choice1, int *tributeRevealedCards, struct gameState *state, int currentPlayer, int nextPlayer);
+int playCardMine(int choice1, int choice2, int handPos, struct gameState *state, int currentPlayer);
+
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+
 
 #endif
